3d3ded76e183501ae7da25b15cf9957ad206b5ac
Lars Petersen <info@lars-petersen.net>
2016-05-06 19:26:25 +0200

    Removed unused dependencies.

--- src/Network/MQTT/Message.hs
+++ src/Network/MQTT/Message.hs
@@ -2,20 +2,18 @@
 module Network.MQTT.Message where
 
 import Control.Applicative
-import Control.Concurrent.Chan
 import Control.Concurrent.MVar
 import Control.Exception
 import Control.Monad.Catch (MonadThrow (..))
 import Control.Monad
 
-import qualified Data.Attoparsec.ByteString as A
-
 import Data.Monoid
 import Data.Bits
 import Data.Function (fix)
 import Data.String
-import qualified Data.Source as S
-import qualified Data.Source.ByteString as S
+import Data.Word
+import Data.Typeable
+import qualified Data.Attoparsec.ByteString as A
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Builder as BS
 import qualified Data.ByteString.Lazy as LBS
@@ -23,10 +21,6 @@ import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Data.Text.Lazy as LT
 import qualified Data.Text.Lazy.Encoding as LT
-import Data.Word
-import Data.Typeable
-
-import Prelude
 
 import Network.MQTT.Message.Blob
 import Network.MQTT.Message.RemainingLength
