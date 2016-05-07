0a01db6d21e4ca240b2cbe0222fa9128e2a7ac9f
Lars Petersen <info@lars-petersen.net>
2016-05-07 22:47:52 +0200

    BestEffort processing for QoS0.

--- src/Network/MQTT/Server.hs
+++ src/Network/MQTT/Server.hs
@@ -42,47 +42,88 @@ data  MqttSessions
 data  MqttSession
     = MqttSession
       { sessionConnection              :: MVar (Async ())
-      , sessionOutput                  :: MVar (Either RawMessage ())
-      , sessionQoS0Queue               :: BC.BoundedChan Message
-      , sessionQoS1Queue               :: BC.BoundedChan Message
-      , sessionQoS2Queue               :: BC.BoundedChan Message
+      , sessionOutputBuffer            :: MVar RawMessage
+      , sessionBestEffortQueue         :: BC.BoundedChan Message
+      , sessionGuaranteedDeliveryQueue :: BC.BoundedChan Message
+      , sessionInboundPacketState      :: MVar (IM.IntMap InboundPacketState)
+      , sessionOutboundPacketState     :: MVar (IM.IntMap OutboundPacketState)
       , sessionTerminate               :: IO ()
       }
 
 data  Identity
+data  InboundPacketState
+
+data  OutboundPacketState
+   =  NotAcknowledgedPublishQoS1 Message
+   |  NotReceivedPublishQoS2     Message
+   |  NotCompletePublishQoS2     Message
+
+data MConnection
+   = MConnection
+     { msend    :: Message -> IO ()
+     , mreceive :: IO Message
+     , mclose   :: IO ()
+     }
 
 publish :: MqttSession -> Message -> IO ()
 publish session message = case qos message of
   -- For QoS0 messages, the queue will simply overflow and messages will get
   -- lost. This is the desired behaviour and allowed by contract.
   QoS0 ->
-    void $ BC.writeChan (sessionQoS0Queue session) message
+    void $ BC.writeChan (sessionBestEffortQueue session) message
   -- For QoS1 and QoS2 messages, an overflow will kill the connection and
   -- delete the session. We cannot otherwise signal the client that we are
   -- unable to further serve the contract.
-  QoS1 -> do
-    success <- BC.tryWriteChan (sessionQoS1Queue session) message
-    unless success $ sessionTerminate session
-  QoS2 -> do
-    success <- BC.tryWriteChan (sessionQoS2Queue session) message
+  _ -> do
+    success <- BC.tryWriteChan (sessionGuaranteedDeliveryQueue session) message
     unless success $ sessionTerminate session
 
 dispatchConnection :: Connection -> MqttSessions -> IO ()
 dispatchConnection connection ms =
   withConnect $ \clientIdentifier cleanSession keepAlive mwill muser j-> do
+    -- Client sent a valid CONNECT packet. Next, authenticate the client.
     midentity <- sessionsAuthentication ms muser
     case midentity of
+      -- Client authentication failed. Send CONNACK with `NotAuthorized`.
       Nothing -> send $ ConnectAcknowledgement $ Left NotAuthorized
+      -- Cient authenticaion successfull.
       Just identity -> do
+        -- Retrieve session; create new one if necessary.
         (session, sessionPresent) <- getSession ms clientIdentifier
-        withMVar (sessionConnection session) cancel -- Cancel existing connections associated with this session.
-        void $ swapMVar (sessionOutput session) (Left $ ConnectAcknowledgement $ Right sessionPresent)
+        -- Now knowing the session state, we can send the success CONNACK.
+        send $ ConnectAcknowledgement $ Right sessionPresent
+        -- Replace (and shutdown) existing connections.
+        modifyMVar_ (sessionConnection session) $ \previousConnection-> do
+          cancel previousConnection
+          async $ maintainConnection session `finally` close connection
   where
+    -- Tries to receive the first packet and (if applicable) extracts the
+    -- CONNECT information to call the contination with.
     withConnect :: (ClientIdentifier -> CleanSession -> KeepAlive -> Maybe Will -> Maybe (Username, Maybe Password) -> BS.ByteString -> IO ()) -> IO ()
     withConnect  = undefined
 
     send :: RawMessage -> IO ()
     send  = undefined
+
+    maintainConnection :: MqttSession -> IO ()
+    maintainConnection session =
+      processKeepAlive `race_` processInput `race_` processOutput
+        `race_` processBestEffortQueue `race_` processGuaranteedDeliveryQueue
+
+      where
+        processKeepAlive = undefined
+        processInput  = undefined
+        processOutput = undefined
+        processBestEffortQueue = forever $ do
+          message <- BC.readChan (sessionBestEffortQueue session)
+          putMVar (sessionOutputBuffer session) Publish
+            { publishDuplicate = False
+            , publishRetain    = retained message
+            , publishQoS       = Nothing
+            , publishTopic     = topic message
+            , publishBody      = payload message
+            }
+        processGuaranteedDeliveryQueue = undefined
 
 getSession :: MqttSessions -> ClientIdentifier -> IO (MqttSession, SessionPresent)
 getSession mqttSessions clientIdentifier =
