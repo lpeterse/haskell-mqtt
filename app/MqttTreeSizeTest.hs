0b4e4d8d2753165ef427a819cec5ae4a51790422
Lars Petersen <info@lars-petersen.net>
2016-10-24 22:44:09 +0200

    Trying shared Topic data structure.

--- app/MqttTreeSizeTest.hs
+++ app/MqttTreeSizeTest.hs
@@ -4,10 +4,13 @@ import Control.Concurrent
 import Control.DeepSeq
 import Data.IntSet as IS
 import qualified Network.MQTT.RoutingTree as R
+import System.Mem
 
 main :: IO ()
 main  = do
   r <- R.randomTree 7 10 :: IO (R.RoutingTree IS.IntSet)
-  r `seq` print "Evaluated. See memory consumption now!"
+  r `seq` print "Evaluated."
+  performGC
+  print "Performed GC. See memory consumption now!"
   threadDelay 100000000
   print (R.null r)
--- src/Network/MQTT/Topic.hs
+++ src/Network/MQTT/Topic.hs
@@ -2,55 +2,38 @@
 module Network.MQTT.Topic
   ( Filter (..)
   , Topic (..)
-  , createFilterElement
-  , createTopicElement
-  , mWord
+  , PathElement (..)
   ) where
 
-import qualified Data.ByteString                 as BS
-import qualified Data.ByteString.Conversion.From as BS
+import qualified Data.ByteString.Short           as BS
+import qualified Data.ByteString.Short.Internal  as BS
 import qualified Data.List.NonEmpty              as NL
-import           Data.Maybe                      (fromMaybe)
-import qualified Data.Text                       as T
-import qualified Data.Text.Encoding              as T
-import           Data.Word                       (Word64)
 
-newtype Filter = Filter (NL.NonEmpty FilterElement)
-newtype Topic  = Topic (NL.NonEmpty TopicElement)
+newtype Filter = Filter (NL.NonEmpty PathElement)
+newtype Topic  = Topic (NL.NonEmpty PathElement)
 
-data FilterElement
-  = SmallFilterElement Word64
-  | FilterWildcard     FilterWildcard
-  | LongFilterElement  BS.ByteString
+data PathElement = PathElement {-# UNPACK #-} !Int {-# UNPACK #-} !Int !BS.ShortByteString
   deriving (Show)
 
-data FilterWildcard = Hash | Plus
+instance Eq PathElement where
+  PathElement off len bs == PathElement off' len' bs' =
+    off == off' && len == len' && f len
+    where
+      -- Heuristic: strings share prefixes and are more likely to differ at the end
+      f 0 = True
+      f l = let offoff = l - 1; i = off + offoff in
+        BS.unsafeIndex bs l ==  BS.unsafeIndex bs' l && f offoff
 
-instance Show FilterWildcard where
-  show Hash = show "#"
-  show Plus = show "+"
-
-createFilterElement :: T.Text -> FilterElement
-createFilterElement str
-  | str == "#" = FilterWildcard Hash
-  | str == "+" = FilterWildcard Plus
-  | size <= 8  = SmallFilterElement $ text2Word64 str
-  | otherwise  = LongFilterElement $ T.encodeUtf8 str
-    where size = BS.length $ T.encodeUtf8 str
-
-data TopicElement
-  = SmallTopicElement Word64
-  | LongTopicElement  BS.ByteString
-    deriving (Show)
-
-createTopicElement :: T.Text -> TopicElement
-createTopicElement str
-  | size <= 8  = SmallTopicElement $ text2Word64 str
-  | otherwise  = LongTopicElement $ T.encodeUtf8 str
-    where size = BS.length $ T.encodeUtf8 str
-
-text2Word64 :: T.Text -> Word64
-text2Word64 str = fromMaybe 0 $ BS.fromByteString (T.encodeUtf8 str)
-
-mWord :: String -> Maybe Word64
-mWord str = BS.fromByteString (T.encodeUtf8 $ T.pack str)
+instance Ord PathElement where
+  compare (PathElement off len bs) (PathElement off' len' bs') =
+    case compare off off' of
+      EQ -> case compare len len' of
+        EQ -> f len
+        y  -> y
+      x -> x
+    where
+      f 0 = EQ
+      f l = let offoff = l - 1; i = off + offoff in
+        case compare (BS.unsafeIndex bs l) (BS.unsafeIndex bs' l) of
+          EQ -> f offoff
+          x  -> x
