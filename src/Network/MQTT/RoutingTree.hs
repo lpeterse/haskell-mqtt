40947bfde967a40c40664491e66f4ecb796aaccb
Lars Petersen <info@lars-petersen.net>
2016-10-25 18:58:16 +0200

    Added Auth interface.

--- src/Network/MQTT/RoutingTree.hs
+++ src/Network/MQTT/RoutingTree.hs
@@ -51,92 +51,92 @@ null  (RoutingTree m) = M.null m
 
 singleton :: RoutingTreeValue a => Filter -> a -> RoutingTree a
 singleton (Filter (x:|xs)) a
-  | rtvNull a  = empty
+  | nodeNull a  = empty
   | otherwise = RoutingTree $ M.singleton x $ case xs of
-      []     -> rtvFromTreeAndValue empty a
-      (y:ys) -> rtvFromTree (singleton (Filter $ y:|ys) a)
+      []     -> nodeFromTreeAndValue empty a
+      (y:ys) -> nodeFromTree (singleton (Filter $ y:|ys) a)
 
 insert :: RoutingTreeValue a => Filter -> a -> RoutingTree a -> RoutingTree a
 insert  = insertWith const
 
 insertWith :: RoutingTreeValue a => (a -> a -> a) -> Filter -> a -> RoutingTree a -> RoutingTree a
 insertWith f (Filter (x:|xs)) a (RoutingTree m)
-  | rtvNull a  = RoutingTree m
+  | nodeNull a  = RoutingTree m
   | otherwise = RoutingTree $ M.alter g x m
   where
     g mn = Just $ case xs of
       []     -> case mn of
-        Nothing -> rtvFromTreeAndValue empty a
-        Just n  -> rtvFromTreeAndValue (rtvTree n) $ fromMaybe a $ f a <$> rtvValue n
-      (y:ys) -> rtvFromTree $ insertWith f (Filter $ y:|ys) a $ fromMaybe empty $ rtvTree <$> mn
+        Nothing -> nodeFromTreeAndValue empty a
+        Just n  -> nodeFromTreeAndValue (nodeTree n) $ fromMaybe a $ f a <$> nodeValue n
+      (y:ys) -> nodeFromTree $ insertWith f (Filter $ y:|ys) a $ fromMaybe empty $ nodeTree <$> mn
 
 adjust :: RoutingTreeValue a => (a -> a) -> Filter -> RoutingTree a -> RoutingTree a
 adjust f (Filter (x:|xs)) (RoutingTree m) =
   RoutingTree $ M.update g x m
   where
     g n = case xs of
-      [] -> case rtvValue n of
+      [] -> case nodeValue n of
         Just v  ->
-          let t' = rtvTree n; v' = f v in
-          if null t' && rtvNull v'
+          let t' = nodeTree n; v' = f v in
+          if null t' && nodeNull v'
             then Nothing
-            else Just $ rtvFromTreeAndValue t' v'
-        Nothing -> Just n -- rtvTree is non-empty by invariant
+            else Just $ nodeFromTreeAndValue t' v'
+        Nothing -> Just n -- nodeTree is non-empty by invariant
       (y:ys) ->
-        let t'@(RoutingTree m') = adjust f (Filter $ y:|ys) (rtvTree n) in
-        case rtvValue n of
-          Just v  -> Just $ rtvFromTreeAndValue t' v
-          Nothing -> if M.null m' then Nothing else Just (rtvFromTree t')
+        let t'@(RoutingTree m') = adjust f (Filter $ y:|ys) (nodeTree n) in
+        case nodeValue n of
+          Just v  -> Just $ nodeFromTreeAndValue t' v
+          Nothing -> if M.null m' then Nothing else Just (nodeFromTree t')
 
 delete :: RoutingTreeValue a => Filter -> RoutingTree a -> RoutingTree a
 delete (Filter (x:|xs)) (RoutingTree m) =
   RoutingTree $ M.update g x m
   where
     g n = case xs of
-      [] | null (rtvTree n) -> Nothing
-         | otherwise     -> Just $ rtvFromTree $ rtvTree n
-      y:ys -> let t = delete (Filter $ y:|ys) (rtvTree n) in
-       case rtvValue n of
+      [] | null (nodeTree n) -> Nothing
+         | otherwise     -> Just $ nodeFromTree $ nodeTree n
+      y:ys -> let t = delete (Filter $ y:|ys) (nodeTree n) in
+       case nodeValue n of
          Nothing | null t    -> Nothing
-                 | otherwise -> Just $ rtvFromTree t
-         Just v -> Just $ rtvFromTreeAndValue t v
+                 | otherwise -> Just $ nodeFromTree t
+         Just v -> Just $ nodeFromTreeAndValue t v
 
 map :: (RoutingTreeValue a, RoutingTreeValue b) => (a -> b) -> RoutingTree a -> RoutingTree b
 map f (RoutingTree m) = RoutingTree $ fmap g m
     where
-      g n = let t = map f (rtvTree n) in case rtvValue n of
-        Nothing -> rtvFromTree t
-        Just a  -> let b = f a in if rtvNull b then rtvFromTree t else rtvFromTreeAndValue t b
+      g n = let t = map f (nodeTree n) in case nodeValue n of
+        Nothing -> nodeFromTree t
+        Just a  -> let b = f a in if nodeNull b then nodeFromTree t else nodeFromTreeAndValue t b
 
 unionWith :: (RoutingTreeValue a) => (a -> a -> a) -> RoutingTree a -> RoutingTree a -> RoutingTree a
 unionWith f (RoutingTree m1) (RoutingTree m2) = RoutingTree (M.unionWith g m1 m2)
   where
-    g n1 n2 = h (unionWith f (rtvTree n1) (rtvTree n2)) (rtvValue n1) (rtvValue n2)
-    h t (Just v1) (Just v2) = rtvFromTreeAndValue t (f v1 v2)
-    h t (Just v1)  _        = rtvFromTreeAndValue t v1
-    h t  _        (Just v2) = rtvFromTreeAndValue t v2
-    h t  _         _        = rtvFromTree         t
+    g n1 n2 = h (unionWith f (nodeTree n1) (nodeTree n2)) (nodeValue n1) (nodeValue n2)
+    h t (Just v1) (Just v2) = nodeFromTreeAndValue t (f v1 v2)
+    h t (Just v1)  _        = nodeFromTreeAndValue t v1
+    h t  _        (Just v2) = nodeFromTreeAndValue t v2
+    h t  _         _        = nodeFromTree         t
 
 differenceWith :: RoutingTreeValue a => (a -> a -> a) -> RoutingTree a -> RoutingTree a -> RoutingTree a
 differenceWith f (RoutingTree m1) (RoutingTree m2) = RoutingTree (M.differenceWith g m1 m2)
   where
-    g n1 n2 = k (differenceWith f (rtvTree n1) (rtvTree n2)) (d (rtvValue n1) (rtvValue n2))
+    g n1 n2 = k (differenceWith f (nodeTree n1) (nodeTree n2)) (d (nodeValue n1) (nodeValue n2))
     d (Just v1) (Just v2)              = Just (f v1 v2)
     d (Just v1)  _                     = Just v1
     d  _         _                     = Nothing
     k t Nothing  | null t              = Nothing
-                 | otherwise           = Just (rtvFromTree t)
-    k t (Just v) | null t && rtvNull v = Nothing
-                 | otherwise           = Just (rtvFromTreeAndValue t v)
+                 | otherwise           = Just (nodeFromTree t)
+    k t (Just v) | null t && nodeNull v = Nothing
+                 | otherwise           = Just (nodeFromTreeAndValue t v)
 
 lookupWith :: (RoutingTreeValue a) => (a -> a -> a) -> Topic -> RoutingTree a -> Maybe a
 lookupWith f (Topic (x:|[])) (RoutingTree m) =
   case M.lookup x m of
     Nothing -> Nothing
-    Just n  -> let RoutingTree m' = rtvTree n in
+    Just n  -> let RoutingTree m' = nodeTree n in
       case M.lookup "#" m' of
-        Nothing -> rtvValue n
-        Just n' -> rtvValue n `h` rtvValue n'
+        Nothing -> nodeValue n
+        Just n' -> nodeValue n `h` nodeValue n'
   where
     h (Just v1) (Just v2) = Just (f v1 v2)
     h (Just v1) _         = Just v1
@@ -146,10 +146,10 @@ lookupWith f (Topic (x:|y:zs)) (RoutingTree m) =
   matchComponent `h` matchSingleLevelWildcard `h` matchMultiLevelWildcard
   where
     matchComponent =
-      M.lookup x m >>= lookupWith f (Topic $ y:|zs) . rtvTree
+      M.lookup x m >>= lookupWith f (Topic $ y:|zs) . nodeTree
     matchSingleLevelWildcard =
-      M.lookup "+" m >>= lookupWith f (Topic $ y:|zs) . rtvTree
-    matchMultiLevelWildcard = M.lookup "#" m >>= rtvValue
+      M.lookup "+" m >>= lookupWith f (Topic $ y:|zs) . nodeTree
+    matchMultiLevelWildcard = M.lookup "#" m >>= nodeValue
     h (Just v1) (Just v2) = Just (f v1 v2)
     h (Just v1) _         = Just v1
     h _         (Just v2) = Just v2
@@ -162,7 +162,7 @@ lookupWith f (Topic (x:|y:zs)) (RoutingTree m) =
 randomTree :: Int -> Int -> IO (RoutingTree IS.IntSet)
 randomTree 0     branching = RoutingTree <$> pure mempty
 randomTree depth branching = RoutingTree <$> foldM (\m e->
-  flip (M.insert e) m <$> (rtvFromTreeAndValue
+  flip (M.insert e) m <$> (nodeFromTreeAndValue
   <$> randomTree (depth - 1) branching
   <*> randomSet :: IO (RoutingTreeNode IS.IntSet))) M.empty (take branching elements)
   where
@@ -175,41 +175,51 @@ randomTree depth branching = RoutingTree <$> foldM (\m e->
           if p < 0.25
             then pure accum
             else f (succ i) $! IS.insert i accum
-    elements  :: [BS.ShortByteString]
-    elements =
-      [ "#", "ahsd","ashdkjahsdla","akhd","bdansbdasbd","hfdskjfadshdshf8sd"
-      , "jfdkfs35", "jfds98", "f789sdhvs8vdsvj9sd8jvpsv", "24", "vojs", "+"]
+
+elements  :: [BS.ShortByteString]
+elements =
+  [ "a","b","c","d","e","f","g","h","i","j","k","l","m" ]
 
 --------------------------------------------------------------------------------
--- Specialised rtvTree implemenations using data families
+-- Specialised nodeTree implemenations using data families
 --------------------------------------------------------------------------------
 
 class RoutingTreeValue a where
   data RoutingTreeNode a
-  rtvNull             :: a -> Bool
-  rtvTree             :: RoutingTreeNode a -> RoutingTree a
-  rtvValue            :: RoutingTreeNode a -> Maybe a
-  rtvFromTree         :: RoutingTree a -> RoutingTreeNode a
-  rtvFromTreeAndValue :: RoutingTree a -> a -> RoutingTreeNode a
+  nodeNull             :: a -> Bool
+  nodeTree             :: RoutingTreeNode a -> RoutingTree a
+  nodeValue            :: RoutingTreeNode a -> Maybe a
+  nodeFromTree         :: RoutingTree a -> RoutingTreeNode a
+  nodeFromTreeAndValue :: RoutingTree a -> a -> RoutingTreeNode a
 
 instance RoutingTreeValue IS.IntSet where
   data RoutingTreeNode IS.IntSet
     = IntSetRoutingTreeNode !(RoutingTree IS.IntSet) !IS.IntSet
-  rtvNull                              = IS.null
-  rtvTree (IntSetRoutingTreeNode t _)  = t
-  rtvValue (IntSetRoutingTreeNode _ v) | rtvNull v  = Nothing
+  nodeNull                              = IS.null
+  nodeTree (IntSetRoutingTreeNode t _)  = t
+  nodeValue (IntSetRoutingTreeNode _ v) | nodeNull v = Nothing
                                        | otherwise = Just v
-  rtvFromTree t                        = IntSetRoutingTreeNode t mempty
-  rtvFromTreeAndValue                  = IntSetRoutingTreeNode
+  nodeFromTree t                        = IntSetRoutingTreeNode t mempty
+  nodeFromTreeAndValue                  = IntSetRoutingTreeNode
 
 instance RoutingTreeValue (Identity a) where
   data RoutingTreeNode (Identity a)
     = TreeNode          !(RoutingTree (Identity a))
     | TreeNodeWithValue !(RoutingTree (Identity a)) !(Identity a)
-  rtvNull                             = const False
-  rtvTree  (TreeNode          t  )    = t
-  rtvTree  (TreeNodeWithValue t _)    = t
-  rtvValue (TreeNode          _  )    = Nothing
-  rtvValue (TreeNodeWithValue _ v)    = Just v
-  rtvFromTree                         = TreeNode
-  rtvFromTreeAndValue                 = TreeNodeWithValue
+  nodeNull                             = const False
+  nodeTree  (TreeNode          t  )    = t
+  nodeTree  (TreeNodeWithValue t _)    = t
+  nodeValue (TreeNode          _  )    = Nothing
+  nodeValue (TreeNodeWithValue _ v)    = Just v
+  nodeFromTree                         = TreeNode
+  nodeFromTreeAndValue                 = TreeNodeWithValue
+
+instance RoutingTreeValue Bool where
+  data RoutingTreeNode Bool = BoolNode {-# UNPACK #-} !Int !(RoutingTree Bool)
+  nodeNull                             = not
+  nodeTree  (BoolNode _ t)             = t
+  nodeValue (BoolNode 0 _)             = Just False
+  nodeValue (BoolNode _ _)             = Just True
+  nodeFromTree                         = BoolNode 0
+  nodeFromTreeAndValue t False         = BoolNode 0 t
+  nodeFromTreeAndValue t True          = BoolNode 1 t
