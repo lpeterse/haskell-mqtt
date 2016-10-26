eca5ec6b2c5f4d716a8f982e054b1aa4c931da95
Lars Petersen <info@lars-petersen.net>
2016-10-27 00:24:33 +0200

    Extended Transport.

--- src/Network/MQTT/Transport.hs
+++ src/Network/MQTT/Transport.hs
@@ -14,14 +14,14 @@ class Transport a where
 class Transport a => ServerTransport a where
   data Server a
   data ServerConfiguration a
-  server           :: ServerConfiguration a -> IO (Server a)
+  serverFromConfig :: ServerConfiguration a -> IO (Server a)
   serverStart      :: Server a -> IO ()
   serverStop       :: Server a -> IO ()
 
 class Transport a => ClientTransport a where
   data Client a
   data ClientConfiguration a
-  client           :: ClientConfiguration a -> IO (Client a)
+  clientFromConfig :: ClientConfiguration a -> IO (Client a)
   clientConnect    :: Client a -> IO ()
   clientDisconnect :: Client a -> IO ()
 
@@ -36,6 +36,6 @@ instance Show (TransportException (S.Socket f t S.TCP)) where
 instance (S.Family f, S.Type t) => ServerTransport (S.Socket f t S.TCP) where
   data Server (S.Socket f t S.TCP) = TcpSocketServer (S.Socket f t S.TCP)
   data ServerConfiguration (S.Socket f t S.TCP) = TcpSocketServerConfiguration (S.SocketAddress f)
-  server config = TcpSocketServer <$> S.socket
+  serverFromConfig config = TcpSocketServer <$> S.socket
   serverStart (TcpSocketServer s) = undefined
   serverStop (TcpSocketServer s) = S.close s
