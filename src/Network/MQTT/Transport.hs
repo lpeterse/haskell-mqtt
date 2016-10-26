82aa9ccf28027012ccd88297c8827d18206e2693
Lars Petersen <info@lars-petersen.net>
2016-10-27 00:03:49 +0200

    Extended Transport.

--- src/Network/MQTT/Transport.hs
+++ src/Network/MQTT/Transport.hs
@@ -1,15 +1,30 @@
-{-# LANGUAGE TypeFamilies, FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies, FlexibleInstances, FlexibleContexts #-}
 module Network.MQTT.Transport where
 
 import qualified System.Socket as S
 import qualified System.Socket.Protocol.TCP as S
 
 class Transport a where
-  data Configuration a
+
+class Transport a => ServerTransport a where
   data Server a
-  newServer :: Configuration a -> IO (Server a)
+  data ServerConfiguration a
+  server           :: ServerConfiguration a -> IO (Server a)
+  serverStart      :: Server a -> IO ()
+  serverStop       :: Server a -> IO ()
+
+class Transport a => ClientTransport a where
+  data Client a
+  data ClientConfiguration a
+  client           :: ClientConfiguration a -> IO (Client a)
+  clientConnect    :: Client a -> IO ()
+  clientDisconnect :: Client a -> IO ()
 
 instance (S.Family f, S.Type t) => Transport (S.Socket f t S.TCP) where
-  data Configuration (S.Socket f t S.TCP) = TcpSocketConfiguration (S.SocketAddress f)
-  data Server (S.Socket f t S.TCP) = TcpSocketServer (Configuration (S.Socket f t S.TCP)) (S.Socket f t S.TCP)
-  newServer config = TcpSocketServer config <$> S.socket
+
+instance (S.Family f, S.Type t) => ServerTransport (S.Socket f t S.TCP) where
+  data Server (S.Socket f t S.TCP) = TcpSocketServer (S.Socket f t S.TCP)
+  data ServerConfiguration (S.Socket f t S.TCP) = TcpSocketServerConfiguration (S.SocketAddress f)
+  server config = TcpSocketServer <$> S.socket
+  serverStart (TcpSocketServer s) = undefined
+  serverStop (TcpSocketServer s) = S.close s
