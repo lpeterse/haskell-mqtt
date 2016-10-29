b8983a38bcbcbaa7b38a36ef7ccc17e2afcc8f2c
Lars Petersen <info@lars-petersen.net>
2016-10-29 03:25:42 +0200

    Added IsString instances for Topic and Filter.

--- src/Network/MQTT/TopicFilter.hs
+++ src/Network/MQTT/TopicFilter.hs
@@ -10,15 +10,35 @@ import Control.Monad ( void )
 import qualified Data.ByteString.Short          as BS
 import qualified Data.ByteString.Short.Internal as BS
 import qualified Data.Attoparsec.ByteString     as A
-import qualified Data.List.NonEmpty             as NL
+import Data.List
+import Data.List.NonEmpty           (NonEmpty(..))
 import           Data.String
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
+import qualified Data.Text.Encoding.Error as T
 import Control.Applicative
 import Data.Word
 
-newtype Filter = Filter (NL.NonEmpty BS.ShortByteString) deriving (Eq, Ord, Show)
-newtype Topic  = Topic (NL.NonEmpty BS.ShortByteString) deriving (Eq, Ord, Show)
+newtype Filter = Filter (NonEmpty BS.ShortByteString) deriving (Eq, Ord)
+newtype Topic  = Topic (NonEmpty BS.ShortByteString) deriving (Eq, Ord)
+
+instance Show Topic where
+  show (Topic xs)       = show (Filter xs)
+
+instance Show Filter where
+  show (Filter (x:|xs)) = concat ["\"", intercalate "/" $ f x : map f xs, "\""]
+    where
+      f = T.unpack . T.decodeUtf8With T.lenientDecode . BS.fromShort
+
+instance IsString Topic where
+  fromString s = case A.parseOnly parseTopic (T.encodeUtf8 $ T.pack s) of
+    Left e  -> error e
+    Right t -> t
+
+instance IsString Filter where
+  fromString s = case A.parseOnly parseFilter (T.encodeUtf8 $ T.pack s) of
+    Left e  -> error e
+    Right t -> t
 
 parseTopic :: A.Parser Topic
 parseTopic = (<|> fail "invalid topic") $ Topic <$> do
@@ -26,7 +46,7 @@ parseTopic = (<|> fail "invalid topic") $ Topic <$> do
   level  <- pLevel
   levels <- A.many' (pSlash >> pLevel)
   A.endOfInput
-  pure (level NL.:| levels)
+  pure (level :| levels)
   where
     pSlash      = A.skip (== slash)
     pLevel      = BS.toShort <$> A.takeWhile
@@ -38,7 +58,7 @@ parseFilter = (<|> fail "invalid filter") $ Filter <$> do
   level  <- pHashOrLevel
   levels <- A.many' (pSlash >> pHashOrLevel)
   A.endOfInput
-  pure (level NL.:| levels)
+  pure (level :| levels)
   where
     pSlash       = A.skip (== slash)
     pHashOrLevel = (A.skip (== hash) >> A.endOfInput >> pure hashLevel) <|> pLevel
--- test/TopicFilterTest.hs
+++ test/TopicFilterTest.hs
@@ -2,44 +2,55 @@
 module TopicFilterTest ( tests ) where
 
 import qualified Data.Attoparsec.ByteString as A
-import           Data.List.NonEmpty       ( NonEmpty(..) )
+import qualified Data.ByteString.Short      as BS
+import           Data.List.NonEmpty         (NonEmpty (..))
 import           Data.Monoid
-import qualified Data.Text                as T
-import           Prelude                  hiding (head)
+import qualified Data.Text                  as T
 
 import           Test.Tasty
 import           Test.Tasty.HUnit
-import           Test.Tasty.QuickCheck    as QC
 
-import qualified Network.MQTT.TopicFilter as F
+import qualified Network.MQTT.TopicFilter   as F
 
 tests :: TestTree
 tests = testGroup "TopicFilter"
-  [ testGroup "parseTopic"
-    [ testCase "! \"\""      $ assertEqual "" (Left "Failed reading: invalid topic") $ A.parseOnly F.parseTopic ""
-    , testCase "! \"\\NUL\"" $ assertEqual "" (Left "Failed reading: invalid topic") $ A.parseOnly F.parseTopic "\NUL"
-    , testCase "! \"+\""     $ assertEqual "" (Left "Failed reading: invalid topic") $ A.parseOnly F.parseTopic "+"
-    , testCase "! \"#\""     $ assertEqual "" (Left "Failed reading: invalid topic") $ A.parseOnly F.parseTopic "#"
-    , testCase "  \"/\""     $ assertEqual "" (Right $ F.Topic $ "":|[""])           $ A.parseOnly F.parseTopic "/"
-    , testCase "  \"//\""    $ assertEqual "" (Right $ F.Topic $ "":|["",""])        $ A.parseOnly F.parseTopic "//"
-    , testCase "  \"/a\""    $ assertEqual "" (Right $ F.Topic $ "":|["a"])          $ A.parseOnly F.parseTopic "/a"
-    , testCase "  \"a\""     $ assertEqual "" (Right $ F.Topic $ "a":|[])            $ A.parseOnly F.parseTopic "a"
-    , testCase "  \"a/\""    $ assertEqual "" (Right $ F.Topic $ "a":|[""])          $ A.parseOnly F.parseTopic "a/"
-    , testCase "  \"a/b123\""   $ assertEqual "" (Right $ F.Topic $ "a":|["b123"])   $ A.parseOnly F.parseTopic "a/b123"
+  [ testGroup "Topic"
+    [ testGroup "show"
+      [ testCase "\"/\""       $ assertEqual "" "\"/\""      (show $ F.Topic $ "":|[""])
+      , testCase "\"\x2603\""  $ assertEqual "" "\"\x2603\"" (show $ F.Topic $ BS.pack [0xE2,0x98,0x83]:|[])
+      , testCase "\"a/b/c\""   $ assertEqual "" "\"a/b/c\""  (show $ F.Topic $ "a":|["b","c"])
+      ]
+    , testGroup "parseTopic"
+      [ testCase "! \"\""      $ assertEqual "" (Left "Failed reading: invalid topic") $ A.parseOnly F.parseTopic ""
+      , testCase "! \"\\NUL\"" $ assertEqual "" (Left "Failed reading: invalid topic") $ A.parseOnly F.parseTopic "\NUL"
+      , testCase "! \"+\""     $ assertEqual "" (Left "Failed reading: invalid topic") $ A.parseOnly F.parseTopic "+"
+      , testCase "! \"#\""     $ assertEqual "" (Left "Failed reading: invalid topic") $ A.parseOnly F.parseTopic "#"
+      , testCase "  \"/\""     $ assertEqual "" (Right $ F.Topic $ "":|[""])           $ A.parseOnly F.parseTopic "/"
+      , testCase "  \"//\""    $ assertEqual "" (Right $ F.Topic $ "":|["",""])        $ A.parseOnly F.parseTopic "//"
+      , testCase "  \"/a\""    $ assertEqual "" (Right $ F.Topic $ "":|["a"])          $ A.parseOnly F.parseTopic "/a"
+      , testCase "  \"a\""     $ assertEqual "" (Right $ F.Topic $ "a":|[])            $ A.parseOnly F.parseTopic "a"
+      , testCase "  \"a/\""    $ assertEqual "" (Right $ F.Topic $ "a":|[""])          $ A.parseOnly F.parseTopic "a/"
+      , testCase "  \"a/b123\""   $ assertEqual "" (Right $ F.Topic $ "a":|["b123"])   $ A.parseOnly F.parseTopic "a/b123"
+      ]
     ]
-  , testGroup "parseFilter"
-    [ testCase "! \"\""          $ assertEqual "" (Left "Failed reading: invalid filter")  $ A.parseOnly F.parseFilter ""
-    , testCase "! \"\\NUL\""     $ assertEqual "" (Left "Failed reading: invalid filter")  $ A.parseOnly F.parseFilter "\NUL"
-    , testCase "  \"+\""         $ assertEqual "" (Right $ F.Filter $ "+":|[])             $ A.parseOnly F.parseFilter "+"
-    , testCase "  \"#\""         $ assertEqual "" (Right $ F.Filter $ "#":|[])             $ A.parseOnly F.parseFilter "#"
-    , testCase "! \"#/\""        $ assertEqual "" (Left "Failed reading: invalid filter")  $ A.parseOnly F.parseFilter "#/"
-    , testCase "  \"/\""         $ assertEqual "" (Right $ F.Filter $ "":|[""])            $ A.parseOnly F.parseFilter "/"
-    , testCase "  \"//\""        $ assertEqual "" (Right $ F.Filter $ "":|["",""])         $ A.parseOnly F.parseFilter "//"
-    , testCase "  \"/a\""        $ assertEqual "" (Right $ F.Filter $ "":|["a"])           $ A.parseOnly F.parseFilter "/a"
-    , testCase "  \"a\""         $ assertEqual "" (Right $ F.Filter $ "a":|[])             $ A.parseOnly F.parseFilter "a"
-    , testCase "  \"a/\""        $ assertEqual "" (Right $ F.Filter $ "a":|[""])           $ A.parseOnly F.parseFilter "a/"
-    , testCase "  \"a/b\""       $ assertEqual "" (Right $ F.Filter $ "a":|["b"])          $ A.parseOnly F.parseFilter "a/b"
-    , testCase "  \"a/+/c123/#\""   $ assertEqual "" (Right $ F.Filter $ "a":|["+","c123","#"])  $ A.parseOnly F.parseFilter "a/+/c123/#"
-    , testCase "! \"a/+/c123/#/d\"" $ assertEqual "" (Left "Failed reading: invalid filter")     $ A.parseOnly F.parseFilter "a/+/c123/#/d"
+  , testGroup "Filter"
+    [ testGroup "show"
+      [ testCase "\"a/b/c\""       $ assertEqual "" "\"a/b/c\"" (show $ F.Filter $ "a":|["b","c"])
+      ]
+    , testGroup "parseFilter"
+      [ testCase "! \"\""          $ assertEqual "" (Left "Failed reading: invalid filter")  $ A.parseOnly F.parseFilter ""
+      , testCase "! \"\\NUL\""     $ assertEqual "" (Left "Failed reading: invalid filter")  $ A.parseOnly F.parseFilter "\NUL"
+      , testCase "  \"+\""         $ assertEqual "" (Right $ F.Filter $ "+":|[])             $ A.parseOnly F.parseFilter "+"
+      , testCase "  \"#\""         $ assertEqual "" (Right $ F.Filter $ "#":|[])             $ A.parseOnly F.parseFilter "#"
+      , testCase "! \"#/\""        $ assertEqual "" (Left "Failed reading: invalid filter")  $ A.parseOnly F.parseFilter "#/"
+      , testCase "  \"/\""         $ assertEqual "" (Right $ F.Filter $ "":|[""])            $ A.parseOnly F.parseFilter "/"
+      , testCase "  \"//\""        $ assertEqual "" (Right $ F.Filter $ "":|["",""])         $ A.parseOnly F.parseFilter "//"
+      , testCase "  \"/a\""        $ assertEqual "" (Right $ F.Filter $ "":|["a"])           $ A.parseOnly F.parseFilter "/a"
+      , testCase "  \"a\""         $ assertEqual "" (Right $ F.Filter $ "a":|[])             $ A.parseOnly F.parseFilter "a"
+      , testCase "  \"a/\""        $ assertEqual "" (Right $ F.Filter $ "a":|[""])           $ A.parseOnly F.parseFilter "a/"
+      , testCase "  \"a/b\""       $ assertEqual "" (Right $ F.Filter $ "a":|["b"])          $ A.parseOnly F.parseFilter "a/b"
+      , testCase "  \"a/+/c123/#\""   $ assertEqual "" (Right $ F.Filter $ "a":|["+","c123","#"])  $ A.parseOnly F.parseFilter "a/+/c123/#"
+      , testCase "! \"a/+/c123/#/d\"" $ assertEqual "" (Left "Failed reading: invalid filter")     $ A.parseOnly F.parseFilter "a/+/c123/#/d"
+      ]
     ]
   ]
